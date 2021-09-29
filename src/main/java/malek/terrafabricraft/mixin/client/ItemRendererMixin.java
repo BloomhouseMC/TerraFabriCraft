9dfa18a02851cdbaf25011dd46360ee4fa719ab8
MalekiRe <pocmalek@gmail.com>
2021-09-28 17:32:41 -0700

    created our own render layer with a crap ton of AW stuff

--- src/main/java/malek/terrafabricraft/mixin/client/ItemRendererMixin.java
+++ src/main/java/malek/terrafabricraft/mixin/client/ItemRendererMixin.java
@@ -3,10 +3,7 @@ package malek.terrafabricraft.mixin.client;
 import net.minecraft.block.Block;
 import net.minecraft.block.StainedGlassPaneBlock;
 import net.minecraft.block.TransparentBlock;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderLayers;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
@@ -16,8 +13,11 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -29,6 +29,35 @@ public class ItemRendererMixin {
     /**
      * @author - MalekiRe
      */
+    @Shadow
+    @Final
+   static RenderPhase.WriteMaskState COLOR_SHADER;
+    @Shadow
+    @Final
+    static RenderPhase.Shader GLINT_SHADER;
+
+    private static RenderLayer GLINT;
+
+    @Shadow
+    @Final
+    static RenderPhase.Cull DISABLE_CULLING;
+
+    @Shadow
+    @Final
+    static RenderPhase.DepthTest EQUAL_DEPTH_TEST;
+
+    @Shadow
+    @Final
+    static RenderPhase.Transparency GLINT_TRANSPARENCY;
+
+    @Shadow
+    @Final
+    static RenderPhase.Texturing GLINT_TEXTURING;
+
+    static {
+            GLINT = RenderLayer.of("glint",VertexFormats.POSITION_TEXTURE, VertexFormat.DrawMode.QUADS, 256,RenderLayer.MultiPhaseParameters.builder().shader(GLINT_SHADER).texture(new RenderPhase.Texture(ItemRenderer.ENCHANTED_ITEM_GLINT, true, false)).writeMaskState(COLOR_SHADER).cull(DISABLE_CULLING).depthTest(EQUAL_DEPTH_TEST).transparency(GLINT_TRANSPARENCY).texturing(GLINT_TEXTURING).build(false));
+
+    }
     @Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At("HEAD"), cancellable = true)
     public void renderItemMixin(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
         if(!stack.isEmpty()) {
--- src/main/resources/terrafabricraft.aw
+++ src/main/resources/terrafabricraft.aw
@@ -2,3 +2,14 @@ accessWidener    v1    named
 accessible class net/minecraft/world/gen/feature/ConfiguredFeatures$Decorators
 accessible class net/minecraft/client/render/RenderLayer
 accessible   method    net/minecraft/client/render/RenderLayer    of    (Ljava/lang/String;Lnet/minecraft/client/render/VertexFormat;Lnet/minecraft/client/render/VertexFormat$DrawMode;IZZLnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;)Lnet/minecraft/client/render/RenderLayer$MultiPhase;
+accessible    class    net/minecraft/client/render/RenderPhase$WriteMaskState
+accessible    class    net/minecraft/client/render/RenderLayer$MultiPhaseParameters
+accessible    class    net/minecraft/client/render/RenderPhase$Shader
+accessible    class    net/minecraft/client/render/RenderPhase$Texture
+accessible    class    net/minecraft/client/render/RenderPhase$Cull
+accessible    class    net/minecraft/client/render/RenderPhase$DepthTest
+accessible    class    net/minecraft/client/render/RenderPhase$Texturing
+accessible    class    net/minecraft/client/render/RenderPhase$Transparency
+accessible    method    net/minecraft/client/render/RenderLayer    of    (Lnet/minecraft/client/render/RenderPhase$Shader;)Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;
+accessible    method    net/minecraft/client/render/RenderLayer    of    (Ljava/lang/String;Lnet/minecraft/client/render/VertexFormat;Lnet/minecraft/client/render/VertexFormat$DrawMode;IZZLnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;)Lnet/minecraft/client/render/RenderLayer$MultiPhase;
+accessible    method    net/minecraft/client/render/RenderLayer    of    (Ljava/lang/String;Lnet/minecraft/client/render/VertexFormat;Lnet/minecraft/client/render/VertexFormat$DrawMode;ILnet/minecraft/client/render/RenderLayer$MultiPhaseParameters;)Lnet/minecraft/client/render/RenderLayer$MultiPhase;
