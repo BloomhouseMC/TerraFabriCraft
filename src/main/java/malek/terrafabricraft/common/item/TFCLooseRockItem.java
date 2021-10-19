43ab9b233872a333746f112dbc56ecc65a298720
Voleil <71479859+Voleil@users.noreply.github.com>
2021-10-19 16:19:10 -0300

    project builds*

--- src/main/java/malek/terrafabricraft/common/item/TFCLooseRockItem.java
+++ src/main/java/malek/terrafabricraft/common/item/TFCLooseRockItem.java
@@ -8,17 +8,19 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 
-public class TFCLooseRockItem extends BlockItem implements ImplementedInventory  {
+public class TFCLooseRockItem extends BlockItem implements ImplementedInventory {
     private DefaultedList<ItemStack> list = DefaultedList.ofSize(1, ItemStack.EMPTY);
 
     public TFCLooseRockItem(Block block, Settings settings) {
@@ -26,25 +28,8 @@ public class TFCLooseRockItem extends BlockItem implements ImplementedInventory 
 
     }
 
-    @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        if(context.getPlayer().isSneaking()){
-            return super.useOnBlock(context);
-        }
-        return ActionResult.PASS;
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!user.isSneaking()) {
-            openScreen(user, user.getStackInHand(hand));
-            return TypedActionResult.success(user.getStackInHand(hand));
-        }
-        return TypedActionResult.pass(user.getStackInHand(hand));
-    }
-
     public static void openScreen(PlayerEntity player, ItemStack looseRockItemStack) {
-        if(player.world != null && !player.world.isClient) {
+        if (player.world != null && !player.world.isClient) {
             player.openHandledScreen(new ExtendedScreenHandlerFactory() {
                 @Override
                 public void writeScreenOpeningData(ServerPlayerEntity serverPlayerEntity, PacketByteBuf packetByteBuf) {
@@ -62,6 +47,23 @@ public class TFCLooseRockItem extends BlockItem implements ImplementedInventory 
                 }
             });
         }
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if (context.getPlayer().isSneaking()) {
+            return super.useOnBlock(context);
+        }
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!user.isSneaking()) {
+            openScreen(user, user.getStackInHand(hand));
+            return TypedActionResult.success(user.getStackInHand(hand));
+        }
+        return TypedActionResult.pass(user.getStackInHand(hand));
     }
 
     @Override
